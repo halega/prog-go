b25dc8ba0484d5c891e510d717f502ca5dc679d7
Stanislav Kalashnikov <stas@halega.com>
2020-11-30 23:40:08 +0300

    json-partial-modify: add test

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/halega/prog-go
 go 1.15
 
 require (
+	github.com/google/go-cmp v0.5.4
 	github.com/mitchellh/mapstructure v1.4.0
 	github.com/tidwall/gjson v1.6.3
 	github.com/tidwall/pretty v1.0.2
--- json-partial-modify/main_test.go
+++ json-partial-modify/main_test.go
@@ -1,6 +1,82 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+const resultS = `{
+	"id": 1,
+	"name": "Agent Emulator",
+	"url": "https://jira.com/agent",
+	"createdAt": "2020-11-30T17:36:00Z",
+	"tasks": [
+		{
+			"taskId": 1,
+			"url": "https://jira.com/agent/1"
+		},
+		{
+			"taskId": 3,
+			"url": "https://jira.com/agent/3"
+		},
+		{
+			"taskId": 5,
+			"url": "https://jira.com/agent/5"
+		},
+		{
+			"taskId": 2,
+			"url": "https://jira.com/agent/2"
+		}
+	]
+}`
+
+type jsonStruct struct {
+	ID        int
+	Name      string
+	URL       string
+	CreatedAt string
+	Tasks     []struct {
+		TaskID int
+		URL    string
+	}
+}
+
+func TestAll(t *testing.T) {
+	funcs := []struct {
+		f    func() ([]byte, error)
+		name string
+	}{
+		{f: byStd, name: "byStd"},
+		{f: byMapStructure, name: "byMapStructure"},
+		{f: byMapStructurePartial, name: "byMapStructurePartial"},
+		{f: bySjson, name: "bySjson"},
+	}
+
+	want := unmarshal([]byte(resultS))
+	for _, f := range funcs {
+		newS, err := f.f()
+		if err != nil {
+			t.Errorf("Error on %s(): %s", f.name, err)
+		}
+		got := unmarshal(newS)
+
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("%s() mismatch (-want +got):\n%s", f.name, diff)
+		}
+
+	}
+
+}
+
+func unmarshal(data []byte) *jsonStruct {
+	var s jsonStruct
+	if err := json.Unmarshal(data, &s); err != nil {
+		panic(err)
+	}
+	return &s
+}
 
 func BenchmarkByStd(b *testing.B) {
 	var result []byte
