b981d31a1bbd29d66ce0f71e1cec9a2348297107
Stanislav Kalashnikov <stas@halega.com>
2022-01-07 12:17:13 +0300

    add Wengrow bubble sort

--- algs/bubble.go
+++ algs/bubble.go
@@ -10,11 +10,12 @@ import "fmt"
 // [1, 5, 7, 3, 9]
 // [1, 3, 7, 5, 9]
 // ...
-func BubbleSort(nums []int) {
+func BubbleSort(nums []int) string {
+	trace := ""
 	steps := 0
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < len(nums)-1; i++ {
 		for j := i + 1; j < len(nums); j++ {
-			fmt.Printf("%4d: %2v\tcompare(%2d, %2d): %2d > %2d\n", steps, nums, i, j, nums[i], nums[j])
+			trace += fmt.Sprintf("%4d: %2v\tcompare(%2d, %2d): %2d > %2d\n", steps, nums, i, j, nums[i], nums[j])
 			steps++ // 1 step for comparinson
 			if nums[i] > nums[j] {
 				steps++ // 1 step for swap
@@ -22,4 +23,27 @@ func BubbleSort(nums []int) {
 			}
 		}
 	}
+	return trace
+}
+
+// Wengrow, A Common-Sense Guide to Data Structures and Algorithms
+// Bubble Sort, Chapter 4. Speeding Up Your Code with Big O.
+// Returns algorithm trace.
+func WengrowBubbleSort(list []int) string {
+	trace := "" // log of each algorithm step
+	steps := 0
+	swapped := true
+	for swapped {
+		swapped = false
+		for i, j := 0, 1; i < len(list)-1; i, j = i+1, j+1 {
+			trace += fmt.Sprintf("%4d: %2v\tcompare(%2d, %2d): %2d > %2d %t\n", steps, list, i, j, list[i], list[j], swapped)
+			steps++
+			if list[i] > list[j] {
+				steps++
+				list[i], list[j] = list[j], list[i]
+				swapped = true
+			}
+		}
+	}
+	return trace
 }
--- algs/bubble_test.go
+++ algs/bubble_test.go
@@ -9,7 +9,7 @@ import (
 func TestBubbleSort4(t *testing.T) {
 	nums := []int{5, 3, 7, 1}
 	sorted := []int{1, 3, 5, 7}
-	if BubbleSort(nums); !cmp.Equal(nums, sorted) {
+	if t.Log(BubbleSort(nums)); !cmp.Equal(nums, sorted) {
 		t.Errorf("wrong sort results; diff:\n%s", cmp.Diff(nums, sorted))
 	}
 }
@@ -17,7 +17,7 @@ func TestBubbleSort4(t *testing.T) {
 func TestBubbleSort8(t *testing.T) {
 	nums := []int{5, 3, 7, 1, 9, 4, 11, 17}
 	sorted := []int{1, 3, 4, 5, 7, 9, 11, 17}
-	if BubbleSort(nums); !cmp.Equal(nums, sorted) {
+	if t.Log(BubbleSort(nums)); !cmp.Equal(nums, sorted) {
 		t.Errorf("wrong sort results; diff:\n%s", cmp.Diff(nums, sorted))
 	}
 }
@@ -25,7 +25,25 @@ func TestBubbleSort8(t *testing.T) {
 func TestBubbleSort16(t *testing.T) {
 	nums := []int{5, 3, 7, 1, 9, 4, 11, 17, 2, 12, 13, 8, 0, 14, 15, -1}
 	sorted := []int{-1, 0, 1, 2, 3, 4, 5, 7, 8, 9, 11, 12, 13, 14, 15, 17}
-	if BubbleSort(nums); !cmp.Equal(nums, sorted) {
+	if t.Log(BubbleSort(nums)); !cmp.Equal(nums, sorted) {
+		t.Errorf("wrong sort results; diff:\n%s", cmp.Diff(nums, sorted))
+	}
+}
+
+// Tests for bubble sort from the book Wengrow.
+
+func TestWengrowBubbleSort4(t *testing.T) {
+	nums := []int{5, 3, 7, 1}
+	sorted := []int{1, 3, 5, 7}
+	if t.Log(WengrowBubbleSort(nums)); !cmp.Equal(nums, sorted) {
+		t.Errorf("wrong sort results; diff:\n%s", cmp.Diff(nums, sorted))
+	}
+}
+
+func TestWengrowBubbleSort16(t *testing.T) {
+	nums := []int{5, 3, 7, 1, 9, 4, 11, 17, 2, 12, 13, 8, 0, 14, 15, -1}
+	sorted := []int{-1, 0, 1, 2, 3, 4, 5, 7, 8, 9, 11, 12, 13, 14, 15, 17}
+	if t.Log(WengrowBubbleSort(nums)); !cmp.Equal(nums, sorted) {
 		t.Errorf("wrong sort results; diff:\n%s", cmp.Diff(nums, sorted))
 	}
 }
