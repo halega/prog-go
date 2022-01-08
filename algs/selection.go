c46a2ee3e36c2d8b050e5b0cf4add671bcfe09e3
Stanislav Kalashnikov <stas@halega.com>
2022-01-08 14:26:45 +0300

    fix SelectionSort

--- algs/selection.go
+++ algs/selection.go
@@ -2,8 +2,9 @@ package algs
 
 // SelectionSort sorts a slice in-place in ascending order
 // using selection sort with time complexity of O(N^2).
+// Data movement is minimal: O(N).
 func SelectionSort(nums []int) {
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < len(nums)-1; i++ {
 		minValIndex := i
 		for j := i + 1; j < len(nums); j++ {
 			if nums[j] < nums[minValIndex] {
