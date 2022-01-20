dc65c796e34bab5789de2955ef98cbf182a66c4c
Stanislav Kalashnikov <stas@halega.com>
2022-01-20 22:19:15 +0300

    add daymask app

---
+++ daymask/main.go
@@ -0,0 +1,21 @@
+/*
+	Program daymask explains which days are encoded in daymask. Each bit represents a day.
+*/
+package main
+
+import "fmt"
+
+const daymask = 131072
+
+func main() {
+	days := fmt.Sprintf("%b", daymask)
+	for i := 1; i <= 31; i++ {
+		fmt.Printf("%2d ", i)
+	}
+	fmt.Println()
+	// days only contains '0' or '1', so we can iterate by bytes (not runes)
+	for i := len(days) - 1; i >= 0; i-- {
+		fmt.Printf("%2c ", days[i])
+	}
+	fmt.Println()
+}
