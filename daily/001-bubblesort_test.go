baa063af20d59261028baee87d2ce6fc425cc4c9
Stanislav Kalashnikov <halega@gmail.com>
2022-05-25 10:10:51 +0300

    daily: 001-bubblesort_test

---
+++ daily/001-bubblesort_test.go
@@ -0,0 +1,28 @@
+package daily
+
+import "testing"
+
+// TestBubbleSort1 implements bubble sort.
+// (Wengrow - A Common-Sense Guide to Data Structures and Algorithms)
+func TestBubbleSort1(t *testing.T) {
+	input := []int{7, 9, 3, 5, 2, 4, 4, 0, -8, 12}
+	t.Log("Input:", input)
+	unsortedUntilIndex := len(input) - 1
+	sorted := false
+	steps := 0
+	for !sorted {
+		steps++
+		sorted = true
+		for i := 0; i < unsortedUntilIndex; i++ {
+			steps++
+			if input[i] > input[i+1] {
+				steps++
+				input[i], input[i+1] = input[i+1], input[i]
+				sorted = false
+			}
+		}
+		unsortedUntilIndex--
+	}
+	t.Log("Sorted:", input)
+	t.Log("Steps:", steps)
+}
