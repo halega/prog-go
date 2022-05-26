bd2a7e72c423ed7d0e2fc94b343384d0e1a52216
Stanislav Kalashnikov <halega@gmail.com>
2022-05-26 17:14:34 +0300

    daily: recalling insertion sort

---
+++ daily/003-insertionsort_test.go
@@ -0,0 +1,33 @@
+// Daily practice 26.05.2022.
+package daily
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// InsertionSort sorts the input in ascending order.
+func InsertionSort(input []int) {
+	for i := 1; i < len(input); i++ {
+		removed := input[i]
+		var j int
+		for j = i - 1; j >= 0; j-- {
+			if input[j] > removed {
+				input[j+1] = input[j]
+			} else {
+				break
+			}
+		}
+		input[j+1] = removed
+	}
+}
+
+func TestInsertionSort1(t *testing.T) {
+	input := []int{1, 0, -1, 4, 2, 2, 10, 3, 4}
+	expected := []int{-1, 0, 1, 2, 2, 3, 4, 4, 10}
+	InsertionSort(input)
+	if !cmp.Equal(expected, input) {
+		t.Error("want", expected, "but was", input)
+	}
+}
