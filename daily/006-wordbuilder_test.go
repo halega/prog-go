fe40ddc49ddff9607261e7c533e67dd952a8e876
Stanislav Kalashnikov <halega@gmail.com>
2022-05-28 08:33:49 +0300

    daily/006: buildWordsUniq

--- daily/006-wordbuilder_test.go
+++ daily/006-wordbuilder_test.go
@@ -13,6 +13,32 @@ func buildWords(chars []string) []string {
 	return words
 }
 
+func buildWordsUniq(chars []string) []string {
+	var words []string
+	for _, c1 := range chars {
+		for _, c2 := range chars {
+			if c1 != c2 {
+				words = append(words, c1+c2)
+			}
+		}
+	}
+	return words
+}
+
+func buildWordsUniqIndex(chars []string) []string {
+	var words []string
+	for i := range chars {
+		for j := range chars {
+			if i != j {
+				words = append(words, chars[i]+chars[j])
+			}
+		}
+	}
+	return words
+}
+
 func TestBuildWords(t *testing.T) {
 	t.Log(buildWords([]string{"a", "b", "c", "d"}))
+	t.Log(buildWordsUniq([]string{"a", "b", "c", "d"}))
+	t.Log(buildWordsUniqIndex([]string{"a", "b", "c", "d"}))
 }
