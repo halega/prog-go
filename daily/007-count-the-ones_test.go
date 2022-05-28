dbe22d376c746c559af26a1c40b28eef5c37cd50
Stanislav Kalashnikov <halega@gmail.com>
2022-05-28 10:25:29 +0300

    daily/007: countTheOnesRange func

--- daily/007-count-the-ones_test.go
+++ daily/007-count-the-ones_test.go
@@ -15,11 +15,35 @@ func countTheOnes(array [][]int) int {
 	return count
 }
 
+func countTheOnesRange(array [][]int) int {
+	count := 0
+	for _, arr := range array {
+		for _, n := range arr {
+			if n == 1 {
+				count++
+			}
+		}
+	}
+	return count
+}
+
 func TestCountTheOnes(t *testing.T) {
 	actual := countTheOnes([][]int{
 		{0, 1, 1, 1, 0},
 		{0, 1, 0, 1, 0, 1},
 		{1, 0},
+	})
+	if actual != 7 {
+		t.Error("want 7, but was", actual)
+	}
+}
+
+func TestCountTheOnesRange(t *testing.T) {
+	actual := countTheOnesRange([][]int{
+		{0, 1, 1, 0, 1},
+		{1, 0, 1, 0, 1, 0},
+		{0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0},
 	})
 	if actual != 7 {
 		t.Error("want 7, but was", actual)
