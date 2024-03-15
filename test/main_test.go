34256d0865d01a05fb95d1cd4fef8717439fc783
Stanislav Kalashnikov <Stanislav.Kalashnikov@kaspersky.com>
2024-03-15 08:54:16 +0300

    test: negate tests

--- test/main_test.go
+++ test/main_test.go
@@ -9,3 +9,23 @@ import (
 func TestDate(t *testing.T) {
 	assert.Equal(t, "1984-04-27", "1984-04-27 ", "they should be equal")
 }
+
+func negate(n int) int { return n }
+
+var negateTests = []struct {
+	n    int
+	want int
+}{
+	{0, 0},
+	{1, -1},
+	{-1, 1},
+	{10, -10},
+}
+
+func TestNegate(t *testing.T) {
+	for _, test := range negateTests {
+		if got := negate(test.n); got != test.want {
+			t.Errorf("negate(%d) = %d, want %d", test.n, got, test.want)
+		}
+	}
+}
